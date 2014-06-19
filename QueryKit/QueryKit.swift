b4daded380cfc0e1a4c76a8678a0203d3f8d945f
Kyle Fuller <inbox@kylefuller.co.uk>
2014-06-19 21:23:59 +0100

    Create initial QuerySet structure

--- QueryKit/QueryKit.swift
+++ QueryKit/QueryKit.swift
@@ -8,3 +8,15 @@
 
 import Foundation
 import CoreData
+
+struct QuerySet {
+    let context:NSManagedObjectContext
+    let entityName:String
+
+    // Initialization
+
+    init(_ context:NSManagedObjectContext, _ entityName:String) {
+        self.context = context
+        self.entityName = entityName
+    }
+}
--- QueryKitTests/QueryKitTests.swift
+++ QueryKitTests/QueryKitTests.swift
@@ -9,5 +9,42 @@
 import XCTest
 import QueryKit
 
+func managedObjectModel() -> NSManagedObjectModel {
+    let personEntity = NSEntityDescription()
+    personEntity.name = "Person"
+    personEntity.managedObjectClassName = "Person"
+
+    let personNameAttribute = NSAttributeDescription()
+    personNameAttribute.name = "name"
+    personNameAttribute.attributeType = NSAttributeType.StringAttributeType
+    personNameAttribute.optional = false
+    personEntity.properties = [personNameAttribute]
+
+    let model = NSManagedObjectModel()
+    model.entities = [personEntity]
+
+    return model
+}
+
+func persistentStoreCoordinator() -> NSPersistentStoreCoordinator {
+    let model = managedObjectModel()
+    let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+    persistentStoreCoordinator.addPersistentStoreWithType(NSInMemoryStoreType, configuration: nil, URL: nil, options: nil, error: nil)
+    return persistentStoreCoordinator
+}
+
+class Person : NSManagedObject { }
+
 class QueryKitTests: XCTestCase {
+    var context:NSManagedObjectContext?
+    var queryset:QuerySet?
+
+    override func setUp() {
+        super.setUp()
+
+        context = NSManagedObjectContext()
+        context!.persistentStoreCoordinator = persistentStoreCoordinator()
+
+        queryset = QuerySet(context!, "Person")
+    }
 }
